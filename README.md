ce2de1fd4ac1f6e9c90d1916e6db304dc3da3243
Adeyemi Akinyemi <adeyemiakinyemi01@gmail.com>
2022-08-13 13:33:50 +0100

    Updated

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a fun website i built to troll a couple of my friends, it also helped me get a better understanding of buttons and how to make them functional in javascript with onclick
---
+++ script.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    document.getElementById("btn").style.color = "blue";  
+}
+
+function showImage() {
+    
+}
+
+function showImage(params) {
+    document.getElementById("image").style.display = "block"
+    document.getElementById("picShow").innerHTML = "So you thought it would be an actual helpful offer, you're a clown"
+    document.getElementById("picShows").style.display = "block"
+    document.getElementById("btn").style.display = "none"
+    document.getElementById("head").style.display = "none"
+}
